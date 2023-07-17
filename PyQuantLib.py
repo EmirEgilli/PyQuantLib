60396d5910bc900b1fadd7cf0fafa6de6925e7c8
Emir Egilli <82716676+EmirEgilli@users.noreply.github.com>
2023-07-17 16:55:43 +0300

    Update PyQuantLib.py

--- PyQuantLib.py
+++ PyQuantLib.py
@@ -934,4 +934,4 @@ def beta_hedge(s1, W1, s2, index, start = None, end = None):
           "-------------------------------------------- \n",
           f"Weight for the long stock {s1}: {W1} \n",
           f"Weight for the short stock {s2}: {W2} \n",
-          f"Cumulative return: %{cum_ret[-1].round(2)}")
+          f"Cumulative return: {cum_ret[-1].round(2)}%")
